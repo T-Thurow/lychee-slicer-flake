3b236babb1c07d30657a60f5b19ad1baede67ca7
T-Thurow <t.thurow@craptech.de>
2024-12-01 19:07:03 +0100

    Added Lychee desktop file and icons to package output

--- package.nix
+++ package.nix
@@ -6,13 +6,24 @@
 let
   pname = "lycheeslicer";
   version = "7.1.2";
-in
-appimageTools.wrapType2 {
-  inherit pname version;
   src = fetchurl {
     url = "https://mango-lychee.nyc3.cdn.digitaloceanspaces.com/LycheeSlicer-${version}.AppImage";
     sha256 = "sha256:1dg2hp1jp75widc1a0mrv2lbcr4mx2fyljx2n2nbb35kygblq6py";
   };
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace-fail 'Exec=AppRun' 'Exec=${pname}'
+    cp -r ${appimageContents}/usr/share/icons $out/share
+  '';
+
   meta = with lib; {
     description = "Lychee 3D Slicer Software";
     homepage = "https://mango3d.io/";
